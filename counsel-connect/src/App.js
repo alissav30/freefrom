9a235916584d33767d6ac035b25142947479b3e7
alissav <63168432+alissav30@users.noreply.github.com>
2020-04-28 23:51:53 -0400

    Sign_in
    made app framework and sign in page

--- counsel-connect/src/App.js
+++ counsel-connect/src/App.js
@@ -1,25 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
+import SignIn from "./components/sign-in.js";
 import "./App.css";
 
 function App() {
   return (
     // test comment -Olivia
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+            <SignIn></SignIn>
+        </div>
     </div>
   );
 }
